c237cd4d3faad9a05b9db43cadb3e8df7946df16
DL-Berkey <ruyria@naver.com>
2023-01-12 03:22:03 +0900

    프론트: 풀 전 임시 커밋

--- front/src/page/Home.tsx
+++ front/src/page/Home.tsx
@@ -1,5 +1,17 @@
+import * as Style from "@/style/page/Home-style";
+
 const Home = () => {
-    return <div>hi</div>;
+    return (
+        <div className="content">
+            <div className="inner">
+                <Style.Greeting>
+                    <span>겨울감자</span>님, <br />
+                    어떤 하루를 보내셨나요? 오늘의 마음을 기록해 보세요!
+                </Style.Greeting>
+                <Style.WriteButton>오늘 일기 쓰기</Style.WriteButton>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
--- front/src/style/page/Home-style.ts
+++ front/src/style/page/Home-style.ts
@@ -1,3 +1,29 @@
 import styled from "styled-components";
 
+import * as ButtonStyle from "@/style/common/Button-style";
+
 export const HomeContainer = styled.div``;
+
+export const Greeting = styled.div`
+    position: absolute;
+
+    top: 280px;
+    left: 40px;
+
+    width: 386px;
+    height: 144px;
+
+    font-weight: ${({ theme }) => theme.fonts.weight.light};
+    font-size: ${({ theme }) => theme.fonts.size.title};
+
+    span {
+        font-weight: ${({ theme }) => theme.fonts.weight.bold};
+    }
+`;
+
+export const WriteButton = styled(ButtonStyle.MediumButton)`
+    position: absolute;
+
+    top: 504px;
+    left: 40px;
+`;
