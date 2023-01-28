e1fd4d0be8b1e2d7ca8b07cb1f3a417f943f34d9
DL-Berkey <ruyria@naver.com>
2023-01-28 21:09:47 +0900

    프론트: App.tsx에 불필요한 코드 제거

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,23 +1,9 @@
 import { useDeferredValue } from "react";
-import { BrowserRouter as Router, Route, Routes, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
 import usePeriodOfDay from "./hooks/usePeriodOfDay";
 import DynamicBackground from "./component/common/DynamicBackground";
 import router from "./route";
-// import Main from "@/layout/Main";
-
-// import Home from "./page/Home";
-// import Login from "./page/Login";
-// import Register from "./page/Register/Register";
-// import RecoveryID from "./page/RecoveryID";
-// import RecoveryPW from "./page/RecoveryPW";
-// import User from "./page/User";
-// import Diary from "./page/diary/Diary";
-// import DiaryCalendar from "./page/diary/DiaryCalendar";
-// import DiaryAll from "./page/diary/DiaryAll";
-// import DiaryChat from "./page/diary/DiaryChat";
-// import DiaryUserAnalysis from "./page/diary/DiaryUserAnalysis";
-// import DiaryWrite from "./page/DiaryWrite";
 
 function App() {
     const period = usePeriodOfDay();
@@ -33,38 +19,9 @@ function App() {
     return (
         <>
             <DynamicBackground period={deferredPeriod} />
-
             <RouterProvider router={route} />
         </>
     );
 }
 
 export default App;
-
-// return (
-//     <>
-//         <DynamicBackground period={deferredPeriod} />
-//         <Main period={deferredPeriod}>
-
-//                 <Router>
-//                     <Header />
-//                     <Routes>
-//                         <Route path="/" element={<Home period={deferredPeriod} />} />
-//                         <Route path="/login" element={<Login />} />
-//                         <Route path="/register" element={<Register />} />
-//                         <Route path="/recovery/id" element={<RecoveryID />} />
-//                         <Route path="/recovery/password" element={<RecoveryPW />} />
-//                         <Route path="/user" element={<User />} />
-//                         <Route path="/diary" element={<Diary />}>
-//                             <Route path="calendar" element={<DiaryCalendar />} />
-//                             <Route path="all" element={<DiaryAll />} />
-//                             <Route path="chat" element={<DiaryChat />} />
-//                             <Route path="user/analysis" element={<DiaryUserAnalysis />} />
-//                         </Route>
-//                         <Route path="/diary/write" element={<DiaryWrite />} />
-//                     </Routes>
-//                     <Footer />
-//                 </Router>
-//         </Main>
-//     </>
-// );
