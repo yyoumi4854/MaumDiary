192c158f4af09c168a7cb0e62167c56feef4a985
유민지 <yyoumi4854@gmail.com>
2023-02-08 14:08:58 +0900

    프론트: MonthStatistics.tsx에 달마다 마음 통계 기능 구현

--- front/src/component/diaryCalendar/MonthStatistics.tsx
+++ front/src/component/diaryCalendar/MonthStatistics.tsx
@@ -1,27 +1,47 @@
 import React from "react";
 import dayjs from "dayjs";
 
+import { Diary, Emotion } from "@/types";
 import * as TextStyle from "@/style/common/Text-style";
 import * as Style from "@/style/component/diaryCalendar/MonthStatistics-style";
 
-import Emotion from "@/utils/emotionIcon";
+import EmotionIMG from "@/utils/emotionIcon";
 
 type Props = {
     dayJs: dayjs.Dayjs;
+    data?: Diary[];
 };
 
-const MonthStatistics = ({ dayJs }: Props) => {
-    const now = dayJs;
+interface EmotionCount {
+    [key: Emotion | string]: number;
+}
 
-    let arr = [20, 30, 10, 5, 20, 1, 1, 1];
+const MonthStatistics = ({ dayJs, data }: Props) => {
+    const currentDay = dayJs.clone();
+    const currentEndDate = currentDay.daysInMonth();
+    console.log(currentEndDate);
+
+    let emotionCount: EmotionCount = {};
+
+    if (data) {
+        for (let day in data) {
+            emotionCount[data[day].emotion]
+                ? (emotionCount[data[day].emotion] += 1)
+                : (emotionCount[data[day].emotion] = 1);
+        }
+
+        for (let emtion in emotionCount) {
+            emotionCount[emtion] = (emotionCount[emtion] / currentEndDate) * 100;
+        }
+    }
 
     return (
         <Style.MonthStatisticsContent>
             <TextStyle.MediumText textAlign={"center"}>
-                {now.format("YYYY")}년 {now.format("MM")}월 마음 통계
+                {currentDay.format("YYYY")}년 {currentDay.format("MM")}월 마음 통계
             </TextStyle.MediumText>
 
-            <Style.StatisticsContnet percent={arr}>
+            <Style.StatisticsContnet emotionCount={emotionCount}>
                 <span>자신감</span>
                 <span>신남</span>
                 <span>감사</span>
@@ -33,35 +53,35 @@ const MonthStatistics = ({ dayJs }: Props) => {
             </Style.StatisticsContnet>
 
             <Style.EmotionListContent>
-                <Style.EmotionList url={Emotion.confidence}>
+                <Style.EmotionList url={EmotionIMG.confidence}>
                     <dt>자신감</dt>
                     <dd>20%</dd>
                 </Style.EmotionList>
-                <Style.EmotionList url={Emotion.excitement}>
+                <Style.EmotionList url={EmotionIMG.excitement}>
                     <dt>신남</dt>
                     <dd>20%</dd>
                 </Style.EmotionList>
-                <Style.EmotionList url={Emotion.thanks}>
+                <Style.EmotionList url={EmotionIMG.thanks}>
                     <dt>감사</dt>
                     <dd>20%</dd>
                 </Style.EmotionList>
-                <Style.EmotionList url={Emotion.comport}>
+                <Style.EmotionList url={EmotionIMG.comfort}>
                     <dt>편안</dt>
                     <dd>20%</dd>
                 </Style.EmotionList>
-                <Style.EmotionList url={Emotion.worry}>
+                <Style.EmotionList url={EmotionIMG.worry}>
                     <dt>걱정</dt>
                     <dd>20%</dd>
                 </Style.EmotionList>
-                <Style.EmotionList url={Emotion.sad}>
+                <Style.EmotionList url={EmotionIMG.sad}>
                     <dt>슬픔</dt>
                     <dd>20%</dd>
                 </Style.EmotionList>
-                <Style.EmotionList url={Emotion.hurt}>
+                <Style.EmotionList url={EmotionIMG.hurt}>
                     <dt>상처</dt>
                     <dd>20%</dd>
                 </Style.EmotionList>
-                <Style.EmotionList url={Emotion.angry}>
+                <Style.EmotionList url={EmotionIMG.angry}>
                     <dt>분노</dt>
                     <dd>20%</dd>
                 </Style.EmotionList>
